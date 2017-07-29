b3e65db4e17edd64e006631826854747b18f8325
brian-cooney <bcooney@gmail.com>
2017-07-29 15:48:25 +0100

    first commit variable lecture

---
+++ script.js
@@ -0,0 +1,37 @@
+//Lecture on variables
+/*
+var name = 'John';
+console.log(name);
+
+var lastName = 'Smith';
+console.log(lastName);
+
+var age = '34';
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+*/
+
+// Lecture : variables 2
+
+var name = 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
