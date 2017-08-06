1bafd7e517f735db00139555ef6ec72edfaa9980
brian-cooney <bcooney@gmail.com>
2017-08-06 18:38:24 +0100

    age + height logic challenge

--- script.js
+++ script.js
@@ -14,16 +14,58 @@ console.log(fullAge);
 */
 
 // Lecture : variables 2
+/*
+var name = 'John';
+var age = '34';
 
-var name = 
+var job, isMarried;
 
+job = 'teacher';
+isMarried = 'false';
 
+console.log(name + ' is a ' + age + ' years old ' + job + '. is he married? ' + isMarried + '.');
 
+*/
 
+//Lecture :operators
+// var now = 2016;
+// var birthYear = now - 26;
+// birthYear = now -26 * 2;
+//
+// console.log(birthYear);
+//
+// var age = 16;
+//
+// if(age < 20 ) {
+//   console.log('John is a teenager');
+// }
+//
+//
+//
+// var job = 'teacher';
+//
+// switch (job) {
+//     case 'teacher':
+//       console.log();
+//
+// Challenge 1
+var johnAge = 23;
+var johnHeight = 180;
 
+var friendAge = 30;
+var friendHeight = 170;
 
+johnScore = johnHeight + (johnAge * 5);
+friendScore = friendHeight + (friendAge * 5);
 
+console.log(johnScore);
+console.log(friendScore);
 
+if (johnScore < friendScore) {
+  console.log('john wins the game with ' + johnScore + ' points!');
+} else {
+  console.log('draw');
+}
 
 
 
